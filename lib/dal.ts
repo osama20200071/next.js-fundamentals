e4f0e17c0c659cc07133d7da5cc605f89a8314de
Osama <osamakaram118@gmail.com>
2025-12-16 22:46:07 +0200

    build getUserByEmail dal function

--- lib/dal.ts
+++ lib/dal.ts
@@ -4,3 +4,14 @@ import { eq } from 'drizzle-orm'
 import { cache } from 'react'
 import { issues, users } from '@/db/schema'
 import { mockDelay } from './utils'
+
+// Get user by email
+export const getUserByEmail = async (email: string) => {
+  try {
+    const result = await db.select().from(users).where(eq(users.email, email))
+    return result[0] || null
+  } catch (error) {
+    console.error('Error getting user by email:', error)
+    return null
+  }
+}

16c209c06ead9460177f629a3dfd9bcd1959d72a
Osama <osamakaram118@gmail.com>
2025-12-21 22:45:19 +0200

    upgrade to next js 16

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  experimental: {
-    dynamicIO: true,
-  },
+  cacheComponents: true,
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 }
 

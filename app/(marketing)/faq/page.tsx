d71530f60208064aa1f562e834e87b6925921710
Osama <osamakaram118@gmail.com>
2025-12-21 19:30:24 +0200

    fix little ui style in faq page

--- app/(marketing)/faq/page.tsx
+++ app/(marketing)/faq/page.tsx
@@ -49,7 +49,7 @@ interface FAQItemProps {
 function FAQItem({ question, answer }: FAQItemProps) {
   return (
     <div>
-      <h4 className="text-lg font-semibold mb-2 text-white">{question}</h4>
+      <h4 className="text-lg font-semibold mb-2 text-black/80">{question}</h4>
       <p className="text-gray-400 dark:text-gray-300">{answer}</p>
     </div>
   )

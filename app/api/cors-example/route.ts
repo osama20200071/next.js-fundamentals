5f7a3edb5da8771d3b0dd75008b2843064187436
Osama <osamakaram118@gmail.com>
2025-12-23 18:28:40 +0200

    example for setting custom headers in api route

---
+++ app/api/cors-example/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  return NextResponse.json(
+    { message: 'This endpoint supports CORS' },
+    {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+    }
+  )
+}
+
+export async function OPTIONS() {
+  return NextResponse.json(
+    {},
+    {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+    }
+  )
+}
